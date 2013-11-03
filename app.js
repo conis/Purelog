fb08c7df8f017e78829e775485c3e98e1ba2525d
Conis <conis.yi@gmail.com>
2013-11-04 00:30:38 +0800

    实现http服务的入口

---
+++ app.js
@@ -0,0 +1,25 @@
+var _express = require('express')
+  , _http = require('http')
+  , _path = require('path')
+  , _config = require('./config');
+
+var app = _express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || _config.port);
+  app.use(_express.favicon());
+});
+
+var _purelog = require('./index');
+//获取程序运行的根目录
+_purelog.root = _path.join(__dirname);
+_purelog.config = _config;
+
+_purelog.router.register(_purelog);
+_purelog.storage.register(_purelog);
+//初始化路由
+_purelog.router.initial(app);
+
+_http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
