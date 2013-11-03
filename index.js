a2117909fee88153575740cada64d0335e5b344e
Conis <conis.yi@gmail.com>
2013-11-04 00:32:03 +0800

    index文件作为包的入口，与http想关的内容转移至app.js

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-var _storage = require('./storage')
-    , express = require('express')
-    , http = require('http')
-    , router = require('./router')
-    , _config = require('./config')
-    , cfgRouter = _config.router;
+var _storage = require('./lib/storage')
+  , _router = require('./lib/router');
 
-var init = function(){
-    //创建索引
-   _storage.make();
-    //建立watch
-}
+var purelog = {
+  storage: _storage,
+  router: _router
+};
 
-init();
-
-var app = express();
-
-app.configure(function(){
-    app.set('port', process.env.PORT || _config.port);
-    app.use(express.favicon());
-});
-
-
-app.get(cfgRouter.index, router.index);
-app.get(cfgRouter.article, router.article);
-app.get(cfgRouter.index_page, router.index);
-app.get(cfgRouter.page, router.page);
-app.get('/themes/:theme/*', router.static);
-app.get(cfgRouter.make, router.make);
-app.all('*', router.notfound);
-
-http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
-});
+exports = module.exports = purelog;
