ee5f8c280fedd224e3f0ccbc20cd5da8e08fba4f
Conis <conis.yi@gmail.com>
2013-11-04 22:38:38 +0800

    index作为一个总入口文件，分别导出router/reduce/router/theme

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-var _storage = require('./lib/storage')
-  , _router = require('./lib/router');
+var _pkg = require('./package.json');
 
-var purelog = {
-  storage: _storage,
-  router: _router
+var _purelog = {
+  //存储
+  storage: require('./lib/storage'),
+  //路由
+  router: require('./lib/router'),
+  //reduce
+  reduce: require('./lib/reduce'),
+  //主题
+  theme: require('./lib/theme'),
+  //工具类
+  util: require('./lib/util'),
+  //config由外部传入
+  config: {},
+  //purelog的信息
+  package: {
+    //版本
+    version: _pkg.version,
+    //名称
+    name: 'Purelog',
+    //网址
+    url: 'http://Purelog.org',
+    //作者
+    author: 'Conis'
+  }
 };
 
-exports = module.exports = purelog;
+exports = module.exports = _purelog;
